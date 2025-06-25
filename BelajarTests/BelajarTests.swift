6afb0306246a10276476a4b57d7df3b43c2b2fed
Oktova Yoga Praditia Samosir <oktovaaaaa@gmail.com>
2025-06-25 15:53:28 +0700

    Fix

---
+++ BelajarTests/BelajarTests.swift
@@ -0,0 +1,17 @@
+//
+//  BelajarTests.swift
+//  BelajarTests
+//
+//  Created by Foundation-017 on 13/06/25.
+//
+
+import Testing
+@testable import Belajar
+
+struct BelajarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
